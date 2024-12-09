6f21764736a16dc22cd0cd854ac351910eba27f2
Your NameShibasish <shibasishdas043@gmail.com>
2024-12-09 23:44:10 +0530

    ok

--- p2/tailwind.config.js
+++ p2/tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+
     extend: {
       animation: {
         rotate: "rotate 10s linear infinite",
