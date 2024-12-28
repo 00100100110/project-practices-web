065fb8a0ea4f04edefa98eae784e2c7c830dc079
Shibasish <shibasishdas043@gmail.com>
2024-12-28 23:32:16 +0530

    ok

--- p2/tailwind.config.js
+++ p2/tailwind.config.js
@@ -2,30 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-
-    extend: {
-      animation: {
-        rotate: "rotate 10s linear infinite",
-      },
-      keyframes: {
-        rotate: {
-          "0%": { transform: "rotate(0deg) scale(10)" },
-          "100%": { transform: "rotate(-360deg) scale(10)" },
-        },
-      },
-      fontFamily:{
-        Playwrite:["Playwrite"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-
+}
